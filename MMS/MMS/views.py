f7017f2e6d303a0a69a836d843fdb86404ed6d8c
Abdul Afroz <abdulafroz59963@gmail.com>
2024-06-12 22:17:21 +0530

    Added basic ui and routes

---
+++ MMS/MMS/views.py
@@ -0,0 +1,37 @@
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+#Department views --------------------------------------------------------------------------------------------------- 
+def addDepartment(request):
+    if request.method == 'POST':
+        return render(request, 'department/addDepartment.html')
+    return render(request, 'department/addDepartment.html')
+
+
+#Course views --------------------------------------------------------------------------------------------------- 
+def addCourse(request):
+    if request.method == 'POST':
+        return render(request, 'course/addCourse.html')
+    return render(request, 'course/addCourse.html')
+
+
+#Faculty views --------------------------------------------------------------------------------------------------- 
+def addFaculty(request):
+    if request.method == 'POST':
+        return render(request, 'faculty/addFaculty.html')
+    return render(request, 'faculty/addFaculty.html')
+
+
+
+#Student views ---------------------------------------------------------------------------------------------------
+def addStudent(request):
+    if request.method == 'POST':
+        return render(request, 'student/addStudent.html')
+    return render(request, 'student/addStudent.html')
---
+++ MMS/accounts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Department(models.Model):
+    dept_id =models.AutoField(primary_key=True)
+    dept_name= models.CharField(max_length=100)
+    dept_email= models.CharField(max_length=100, blank=True)
+
+class Course(models.Model):
+    course_id= models.AutoField(primary_key=True)
+    course_name=models.CharField(max_length=100)
+    course_credits= models.FloatField()
+    course_dept= models.ForeignKey(Department, related_name="courseDept",on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+    stud_id= models.AutoField(primary_key=True)
+    stud_name=models.CharField(max_length=100)
+    stud_age= models.IntegerField()
+    stud_email= models.CharField(max_length=100)
+    stud_phone = models.BigIntegerField()
+    stud_dept= models.ForeignKey(Department, related_name="studentDept",on_delete=models.CASCADE)
+
+class Faculty(models.Model):
+    fac_id= models.AutoField(primary_key=True)
+    fac_name=models.CharField(max_length=100)
+    fac_age= models.IntegerField()
+    fac_email= models.CharField(max_length=100)
+    fac_phone = models.BigIntegerField()
+    fac_dept= models.ForeignKey(Department, related_name="facultyDept",on_delete=models.CASCADE)
+
+
+
