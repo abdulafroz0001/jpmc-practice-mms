9a8f7921f3e830f9027c6414c422f6e94d37903f
Harsha <rharsha171@gmail.com>
2024-06-13 14:44:27 +0530

    addDepartment.html updated

--- MMS/MMS/settings.py
+++ MMS/MMS/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hackathon',
         'USER': 'root',
-        'PASSWORD': 'A1a@786afroz',
+        'PASSWORD': 'root',
         'HOST': 'localhost',  # or the hostname where your MySQL server is running
         'PORT': '3306',      # or the port on which your MySQL server is listening
     }
