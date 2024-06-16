cb7adf7e27779b59c23b1c35f0a42c04c348c926
Abdul Afroz <abdulafroz59963@gmail.com>
2024-06-16 18:31:08 +0530

    merged harsha code and srinu code

--- MMS/MMS/views.py
+++ MMS/MMS/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_http_methods
-
+from django.contrib import messages
 from accounts import models
 from django.shortcuts import render, redirect
 
